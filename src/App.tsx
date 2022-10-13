6e692793f5b8c194ea54b62da4584e709544401c
youthh <valavskiylesha630@gmail.com>
2022-10-13 11:40:43 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="inner__app">
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./header.css";
+import headerPeople from "../../images/headerPeople.svg";
+const Header = () => {
+  return (
+    <div className="header__box">
+      <div>d</div>
+      <div>
+        <img src={headerPeople} alt="people" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
